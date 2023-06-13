46c72124b72417f3599cde9b42148b688d9f355d
Mitch Routt <mitchroutt@gmail.com>
2023-06-13 15:49:18 +0200

    Added About Me section to home page.

---
+++ force-app/main/default/lwc/aboutMe/aboutMe.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+
+export default class AboutMe extends LightningElement {
+
+    aboutMeImageURL = IMAGES + '/about-me.png';
+}
